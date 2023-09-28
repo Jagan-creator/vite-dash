31f008a18b80325c6487a1accaacd7c7632ae900
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-27 19:49:02 -0600

    import navbar into app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+import Navbar from "./components/Navbar";
 import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <main className="flex">
       <Sidebar />
-      <div>Components</div>
+      <div className="flex flex-col flex-1 relative">
+        <Navbar />
+      </div>
     </main>
   );
 }
