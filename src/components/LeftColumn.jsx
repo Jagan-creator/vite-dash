73777dcd220fc845355425ad22d8ad66ca91b2ed
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-30 19:01:54 -0600

    create baseline for left and right columns

---
+++ src/components/LeftColumn.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function LeftColumn() {
+  return (
+    <div className="w-full flex flex-col justify-between p-2">
+      <div className="flex flex-col lg:flex-row gap-2 w-full">
+        <p>Card Items</p>
+        <p>Card Items</p>
+        <p>Card Items</p>
+      </div>
+    </div>
+  );
+}
+
+export default LeftColumn;
---
+++ src/components/RightColumn.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RightColumn() {
+  return <div>RightColumn</div>;
+}
+
+export default RightColumn;
