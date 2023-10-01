31c3d95c5c88a14266a99191af00091a57d81c86
Joel Hagan <joel.hagan.8@gmail.com>
2023-09-30 19:22:48 -0600

    implement area chart in left column

--- src/components/LeftColumn.jsx
+++ src/components/LeftColumn.jsx
@@ -1,5 +1,43 @@
 import React from "react";
 import CardItem from "./CardItem";
+import { AreaChart, Card, Title } from "@tremor/react";
+
+const chartdata = [
+  {
+    date: "Jan 22",
+    SemiAnalysis: 2890,
+    "The Pragmatic Engineer": 2338,
+  },
+  {
+    date: "Feb 22",
+    SemiAnalysis: 2756,
+    "The Pragmatic Engineer": 2103,
+  },
+  {
+    date: "Mar 22",
+    SemiAnalysis: 3322,
+    "The Pragmatic Engineer": 2194,
+  },
+  {
+    date: "Apr 22",
+    SemiAnalysis: 3470,
+    "The Pragmatic Engineer": 2108,
+  },
+  {
+    date: "May 22",
+    SemiAnalysis: 3475,
+    "The Pragmatic Engineer": 1812,
+  },
+  {
+    date: "Jun 22",
+    SemiAnalysis: 3129,
+    "The Pragmatic Engineer": 1726,
+  },
+];
+
+const dataFormatter = (number) => {
+  return "$ " + Intl.NumberFormat("us").format(number).toString();
+};
 
 function LeftColumn() {
   return (
@@ -8,6 +46,19 @@ function LeftColumn() {
         <CardItem />
         <CardItem />
         <CardItem />
+      </div>
+      <div className="flex-auto w-full">
+        <Card className="mt-2">
+          <Title>Newsletter revenue over time (USD)</Title>
+          <AreaChart
+            className="h-72 mt-4"
+            data={chartdata}
+            index="date"
+            categories={["SemiAnalysis", "The Pragmatic Engineer"]}
+            colors={["indigo", "cyan"]}
+            valueFormatter={dataFormatter}
+          />
+        </Card>
       </div>
     </div>
   );
