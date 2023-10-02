0eb9167fa57ddeae037d267a07c034866386f1d5
Joel Hagan <joel.hagan.8@gmail.com>
2023-10-02 14:33:16 -0600

    adjust spacing for main container

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import LeftColumn from "./components/LeftColumn";
 import Navbar from "./components/Navbar";
 import RightColumn from "./components/RightColumn";
@@ -6,10 +7,12 @@ import Sidebar from "./components/Sidebar";
 function App() {
   return (
     <main className="flex">
-      <Sidebar />
+      <div className="bg-gray-900 p-1 sm:p-0">
+        <Sidebar />
+      </div>
       <div className="flex flex-col flex-1 relative">
         <Navbar />
-        <div className="grid md:grid-cols-3 grid-cols-1 w-full">
+        <div className="grid md:grid-cols-3 grid-cols-1 w-full px-2">
           <div className="col-span-2">
             <LeftColumn />
           </div>
