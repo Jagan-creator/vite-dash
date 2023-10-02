bb2e310840f4d9db98ec7e1fccd2e58caa24066c
Joel Hagan <joel.hagan.8@gmail.com>
2023-10-02 14:57:45 -0600

    import score list into right column

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import SalesItem from "./SalesItem";
 import WebAnalytics from "./WebAnalytics";
+import ScoreList from "./ScoreList";
 
 function RightColumn() {
   return (
     <div className="w-full pl-2 mt-2">
       <SalesItem />
       <WebAnalytics />
+      <ScoreList />
     </div>
   );
 }
