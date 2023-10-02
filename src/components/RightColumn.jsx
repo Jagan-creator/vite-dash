53f4d7adbb547a5da7681aedc80968a273a48dcb
Joel Hagan <joel.hagan.8@gmail.com>
2023-10-02 13:29:30 -0600

    import sales item into right column

--- src/components/RightColumn.jsx
+++ src/components/RightColumn.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import SalesItem from "./SalesItem";
 
 function RightColumn() {
-  return <div>RightColumn</div>;
+  return (
+    <div className="w-full p-2">
+      <SalesItem />
+    </div>
+  );
 }
 
 export default RightColumn;
